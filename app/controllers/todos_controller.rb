4e0f0a0565cf5b05f08f64b160373462fd5098f1
Todd Utterback <todd.j.utterback@gmail.com>
2016-03-01 15:08:23 -0700

    different pages, edit lists, show lists

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -12,7 +12,7 @@ class TodosController < ApplicationController
   end
 
   def create
-    @tod = Todo.new(todo_params)
+    @todo = Todo.new(todo_params)
     if @todo.save
       redirect_to todo_path
     else
@@ -20,5 +20,31 @@ class TodosController < ApplicationController
     end
   end
 
+  def edit
+    @todo = Todo.find(params[:id])
+  end
+
+    def update
+    @todo = Todo.find(params[:id])
+
+    if @todo.update(todo_params)
+      redirect_to todos_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy
+    redirect_to todos_path
+  end
+
+
+private
   
+  def todo_params
+    params.require(:todo).permit(:title, :priority, :body)
+  end
+
 end
